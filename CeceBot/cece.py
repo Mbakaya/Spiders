b88cd2458aa7fe1a0300332845250c35875d7421
Mbakaya <officialmbakaya@gmail.com>
2017-04-05 09:04:40 +0300

    Add files via upload

---
+++ CeceBot/cece.py
@@ -0,0 +1,12 @@
+
+#!usr/bin/python
+
+print "hi handsome"
+ 
+babe = raw_input("What book are you reading now?")
+print('hehe i have read this'+ babe +'book, ni poa sana')
+
+print " ilove the books youre reading, youre amazing "
+raw_input()
+
+print "of course you are"
